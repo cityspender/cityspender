c8f161b50c97ba2df2b63212fd52a945f7838098
Ravinder Jilkapally <jravinderreddy@gmail.com>
2010-12-09 11:59:28 -0500

    modified the csv uploader

--- webapp/grails/cityspender-webapp/grails-app/controllers/cityspender/webapp/ApplicationController.groovy
+++ webapp/grails/cityspender-webapp/grails-app/controllers/cityspender/webapp/ApplicationController.groovy
@@ -7,14 +7,16 @@ class ApplicationController {
 
     def index = { }
     def uploadPCT = {
-        sdf.set2DigitYearStart(new GregorianCalendar(2001,1,1).getTime())
-        new File( 'c:\\pctnewest.csv' ).text.eachLine{r,rNumber->
+        //sdf.set2DigitYearStart(new GregorianCalendar(2001,1,1).getTime())
+        new File( servletContext.getRealPath("/")+'/data/pctnewest.csv' ).text.eachLine{r,rNumber->
 
             if(rNumber >= 1)  {
                 r.splitEachLine(","){ line ->
-                    print "\n" +  sdf.parse(line[2])
+                 if(line[0] && line[1] && line[2])
+                   {
+                       print "rNumber \n"+line[0]+"\n"+line[1]+"\n"+line[2]+"\n"
 
-                    def pct = new PurchaseCardTransaction(mchMerchantName:line[0], finTransactionAmount:line[1] as Float, finTransactionDate:sdf.parse(line[2]))
+                    def pct = new PurchaseCardTransaction(mchMerchantName:line[0], finTransactionAmount:line[1] as Float, finTransactionDate:sdf.parse(line[2]) as Date)
                     pct.save()
                     def merchant = Merchant.findWhere(mchMerchantName:line[0])
 
@@ -30,16 +32,16 @@ class ApplicationController {
                         transaction = new Transaction(merchant:merchant, finTransactionAmount:line[1] as Float, finTransactionDate:sdf.parse(line[2]))
                         transaction.save()
                     }
-                    else
+                     if(pct?.errors || transaction?.errors || merchant?.errors)
 
                     print "\n pct error "+pct?.errors+"\n transaction error "+transaction?.errors+"\n merchant error "+merchant?.errors
 
-
+                   }
 
                 }
             }
         }
-
+        redirect(controller:"transaction", action:index)
 
     }
 
